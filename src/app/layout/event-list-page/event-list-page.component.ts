5014d1855f56e37f33cdc867c7ff0b1dddcdfc0b
Denis <densiter@gmail.com>
2018-04-03 21:21:39 +0300

    Used localStoradge for navigation

--- src/app/layout/event-list-page/event-list-page.component.ts
+++ src/app/layout/event-list-page/event-list-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-event-list-page',
@@ -8,16 +9,20 @@ import {Component, OnInit} from '@angular/core';
 export class EventListPageComponent implements OnInit {
 
   events = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-  rowsQuantity = this.getRowsQuantity(this.events.length);
 
-  private getRowsQuantity(length) {
-    return length % 4 === 0 ? length / 4 : length / 4 + 1;
-  }
-
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit() {
+   localStorage.setItem('token', 'asdfasdfasdf');
+   // localStorage.removeItem('token');
   }
 
+  goToEvent() {
+    if (localStorage.getItem('token') === null) {
+      this.router.navigate(['login']);
+    } else {
+      this.router.navigate(['event']);
+    }
+  }
 }
